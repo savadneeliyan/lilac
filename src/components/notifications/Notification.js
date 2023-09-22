f1b17acec8a4ab905fae2fc067491d92f624179d
savad neeliyan <84341009+savadneeliyan@users.noreply.github.com>
2023-09-23 01:42:22 +0530

    initial commit

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -8,80 +8,80 @@ function Notification() {
           <img src="/assets/images/edumpus.png" alt="" />
         </div>
         <div>
-          <div className='message'>
+          <div className="message">
             <span className="name">Edumpus</span>
             <span>commented on your post</span>
           </div>
-          <div className="time">3 min ago</div>
+          <div className="time">Now</div>
         </div>
       </div>
       <div className="notification-container active">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/women/91.jpg" alt="" />
         </div>
         <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
+          <div className="message">
+            <span className="name">Snaillogkyanitesnake</span>
+            <span>Applied a new syllabus course as sample text like this is ui</span>
+          </div>
+          <div className="time">5 min ago</div>
+        </div>
+      </div>
+      <div className="notification-container">
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/women/1.jpg" alt="" />
+        </div>
+        <div>
+          <div className="message">
+            <span className="name">Jennifer Lopez</span>
+            <span>applied science and technology degree courses</span>
+          </div>
+          <div className="time">1 hrs ago</div>
+        </div>
+      </div>
+      <div className="notification-container">
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/women/41.jpg" alt="" />
+        </div>
+        <div>
+          <div className="message">
+            <span className="name">Molly Morgan</span>
+            <span>replied your comment</span>
+          </div>
+          <div className="time">1 day ago</div>
+        </div>
+      </div>
+      <div className="notification-container">
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/women/21.jpg" alt="" />
+        </div>
+        <div>
+          <div className="message">
+            <span className="name">Danny Miles</span>
             <span>commented on your post</span>
           </div>
           <div className="time">3 min ago</div>
         </div>
       </div>
       <div className="notification-container">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="" />
         </div>
         <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
+          <div className="message">
+            <span className="name">Haris web design</span>
             <span>commented on your post</span>
           </div>
           <div className="time">3 min ago</div>
         </div>
       </div>
       <div className="notification-container">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
+        <div className="notification-logo ">
+          <img src="https://randomuser.me/api/portraits/men/43.jpg" alt="" />
         </div>
         <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
-            <span>commented on your post</span>
-          </div>
-          <div className="time">3 min ago</div>
-        </div>
-      </div>
-      <div className="notification-container">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
-        </div>
-        <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
-            <span>commented on your post</span>
-          </div>
-          <div className="time">3 min ago</div>
-        </div>
-      </div>
-      <div className="notification-container">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
-        </div>
-        <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
-            <span>commented on your post</span>
-          </div>
-          <div className="time">3 min ago</div>
-        </div>
-      </div>
-      <div className="notification-container">
-        <div className="notification-logo edumpus">
-          <img src="/assets/images/edumpus.png" alt="" />
-        </div>
-        <div>
-          <div className='message'>
-            <span className="name">Edumpus</span>
+          <div className="message">
+            <span className="name">john due</span>
             <span>commented on your post</span>
           </div>
           <div className="time">3 min ago</div>
